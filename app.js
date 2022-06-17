e0dfd9faf75cfa176cf415276516cf0dbf9a8618
romaaCode <80066138+romaaCode@users.noreply.github.com>
2022-06-17 08:57:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn1 = document.querySelector('.btn1');
+
+btn1.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
